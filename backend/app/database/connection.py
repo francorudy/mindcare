5e4c69e181c2f8f9b53bb40ae07d6b9e79e9dc99
franco rudy <u201821279@upc.edu.pe>
2026-06-18 00:03:43 -0500

    Configuracion de variables de entorno con .env

--- backend/app/database/connection.py
+++ backend/app/database/connection.py
@@ -1,13 +1,12 @@
+from dotenv import load_dotenv
 import os
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://postgres:naruto@localhost:5432/mindcare_db",
-)
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
